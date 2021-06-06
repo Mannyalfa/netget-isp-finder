f0bb494949b84d719fbfc2703528a1d8ccbc4e98
Manny Alfaro <78244552+Mannyalfa@users.noreply.github.com>
2021-06-05 21:06:37 -0400

    added search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 https://maps.googleapis.com/maps/api/place/findplacefromtext/output?parameters
 
-APi keyAIzaSyDr0xMEDOlQ6Lal0sfxM954Mh1IXI-V_40
+var apiKey= AIzaSyDr0xMEDOlQ6Lal0sfxM954Mh1IXI-V_40
+var q = "";
 
-const settings = {
+var settings = {
 	"async": true,
 	"crossDomain": true,
 	"url": "https://google-search3.p.rapidapi.com/api/v1/serp/",
@@ -19,3 +20,34 @@ const settings = {
 $.ajax(settings).done(function (response) {
 	console.log(response);
 });
+
+//Setting the click function at ID search button
+$("#search-button").on("click", function (event) {
+	// Invalid City Name Entered
+	event.preventDefault();
+
+	q = $("#city-input").val();
+	if (q === "") {
+		return alert("Please Enter Valid City Name ! ");
+	}
+	getWeather(q);
+
+	saveToLocalStorage(q);
+});
+// Button for searched city
+function createRecentSearchBtn(q) {
+	var newLi = $("<li>");
+	var newBtn = $("<button>");
+	//Adding Extra ID for Button to stop Creating Duplicate Button on Click
+	newBtn.attr("id", "extraBtn");
+	newBtn.addClass("button is-small recentSearch");
+	newBtn.text(q);
+	newLi.append(newBtn);
+	$("#historyList").prepend(newLi);
+	//setting click function to prevent duplicate button
+	$("#extraBtn").on("click", function () {
+		var newQ = $(this).text();
+		getList(newQ);
+
+
+
