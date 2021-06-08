aa4fc5c2db11aba07990312b2c12f95393566e18
Manny Alfaro <78244552+Mannyalfa@users.noreply.github.com>
2021-06-08 18:48:35 -0400

    added more javaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 // API Key
 var APIKey = "AIzaSyDr0xMEDOlQ6Lal0sfxM954Mh1IXI-V_40";
-var q = "Orlando";
+var citySearch = "Orlando";
 
-/*function getBusiness() {*/
+/*get ISP providers by city*/
 var placeSearch =
-	"https://maps.googleapis.com/maps/api/place/textsearch/json?query=internet+servic+provider+in+" + q + "&key=" + APIKey;
+	"https://maps.googleapis.com/maps/api/place/textsearch/json?query=internet+servic+provider+in+" + citySearch + "&key=" + APIKey;
 
 fetch(placeSearch)
 	.then((response) => {
@@ -14,3 +14,25 @@ fetch(placeSearch)
 		console.log(json);
 	
 	}); 
+
+	/*get company info*/
+
+	var reference ="ChIJr5bVFQJ754gRivHFkJhOGJw"
+
+	var placeDetails =
+	"https://maps.googleapis.com/maps/api/place/details/json?place_id=" + reference + "&key=" + APIKey;
+
+fetch(placeDetails)
+	.then((response) => {
+		return response.json();
+	})
+	.then((json) => {
+		console.log(json);
+	
+	}); 
+
+
+	/*q = $("#city-input").val();
+	if (q === "") {
+		return alert("Please Enter Valid City Name ! ");
+	}*/
