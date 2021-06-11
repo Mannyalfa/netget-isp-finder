af97bb389f08bf01b358fd38d048c691689f882b
Bage Gonz <BageGonz@users.noreply.github.com>
2021-06-11 18:03:19 -0400

    added js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,25 @@
-document.getElementById("search-button").addEventListener
-("click", searchButton, placeId);
-
-
-function searchButton () {
+$(window).on("load", function () {
 	
-	fetch("https://api.allorigins.win/raw?url=https%3A//maps.googleapis.com/maps/api/place/textsearch/json%3Fquery%3Dinternet+servic+provider+in+orlando%26key%3DAIzaSyDr0xMEDOlQ6Lal0sfxM954Mh1IXI-V_40&callback=?")
-        .then(response => response.text())
-        .then(response => console.log(JSON.parse(response)))
-		
-		
-
-		
-		
-}
-
-function placeId () {
-	fetch("http://api.allorigins.win/get?url=https%3A//maps.googleapis.com/maps/api/place/details/json%3Fplace_id%3DChIJr5bVFQJ754gRivHFkJhOGJw%26key%3DAIzaSyDr0xMEDOlQ6Lal0sfxM954Mh1IXI-V_40&callback=?")
-        .then(response => response.text())
-        .then(response => console.log(JSON.parse(response)))
-		
-}
-
-
-
-
-
-
-
-
-
-
-/*$(window).on("load", function () {
-
-
+	jQuery.ajaxPrefilter(function (options) {
+		if (options.crossDomain && jQuery.support.cors) {
+			options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+		}
+	});
 	// API Key
 	var APIKey = "AIzaSyDr0xMEDOlQ6Lal0sfxM954Mh1IXI-V_40";
 	var citySearch = $("#search-input").val();
-
 	//Setting the click function at ID search button
 	$("#search-button").on("click", function (event) {
 		// Invalid City Name Entered
 		event.preventDefault();
-		CI = $("#city-input").val();
-		if (citySearch === "") {
+		citySearch = $("#search-input").val();
+		getBusiness(citySearch);
+		/*if (citySearch === "") {
 			//Modal
 			$(document).ready(function () {
 				$('.modal').modal();
 			});
-		}
+		}*/
 	});
 	//get ISP providers by city
 	function getBusiness(citySearch) {
@@ -58,26 +30,29 @@ function placeId () {
 			url: placeFind,
 			method: "GET"
 		})
-			.then(function (data) {
-				console.log(data)
-			}).then(function getDetails() {
-			var placeDetails =
-				"https://maps.googleapis.com/maps/api/place/details/json?place_id=" + reference + "&key=" + APIKey;
-			$.ajax({
-				url: placeDetails,
-				method: "GET"
-
-				//return details and format response in HTML
-			}).then(function (response) {
-				console.log(response);
-				//to avoid repeating city information on button click
-				$("card-action").empty();
-				$("").empty();
-
-				var image = $('<img class="imgsize">').attr("src", "http:");
-				var ispName = $("<p>").text("ISP:" + response.name + "<p>");
-				var phoneNumber = $("<p>").text("Phone: " + response.formatted_phone_number + "<p>");
-				var webAddress = $("<p>").text("Website: " + response.website + "<p>");
-				var custRating = $("<p>").text("Customer rating (1-5): " + response.main.rating + "<p>");
-			})
-		}*/
+			.then(function (response) {
+				console.log((response));
+				var placesArray = response.results;
+				var i;
+				for (i = 0; i < 4; i++) {
+					var placeDetails =
+						"https://maps.googleapis.com/maps/api/place/details/json?place_id=" + placesArray[i].place_id + "&key=" + APIKey;
+					$.ajax({
+						url: placeDetails,
+						method: "GET"
+						//return details and format response in HTML
+					}).then(function (response) {
+						console.log(response);
+						//to avoid repeating city information on button click
+						$("card-action").empty();
+						$("").empty();
+						var image = $('<img class="imgsize">').attr("src", "http:");
+						var ispName = $("<p>").text("ISP:" + response.name + "<p>");
+						var phoneNumber = $("<p>").text("Phone: " + response.formatted_phone_number + "<p>");
+						var webAddress = $("<p>").text("Website: " + response.website + "<p>");
+						var custRating = $("<p>").text("Customer rating (1-5): " + response.main.rating + "<p>");
+					})
+				}
+			});
+	}
+});
