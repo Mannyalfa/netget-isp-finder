f2f023332138c44d18d2764f51f0ee2ea8e31ea7
Manny Alfaro <78244552+Mannyalfa@users.noreply.github.com>
2021-06-12 14:00:03 -0400

    removed second for loop in script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,10 @@
 $(window).on("load", function () {
 
-jQuery.ajaxPrefilter(function (options) {
-	if (options.crossDomain && jQuery.support.cors) {
-		options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
-}
-});
-
-
+	jQuery.ajaxPrefilter(function (options) {
+		if (options.crossDomain && jQuery.support.cors) {
+			options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+		}
+	});
 	// API Key
 	var APIKey = "AIzaSyDr0xMEDOlQ6Lal0sfxM954Mh1IXI-V_40";
 	var citySearch = $("#search-input").val();
@@ -17,14 +15,14 @@ jQuery.ajaxPrefilter(function (options) {
 		event.preventDefault();
 		citySearch = $("#search-input").val();
 		getBusiness(citySearch);
-		/*if (citySearch === "") {
+		if (citySearch === "") {
 			//Modal
 			$(document).ready(function () {
 				$('.modal').modal();
 			});
-		}*/
+		}
 
-		});
+	});
 	//get ISP providers by city
 	function getBusiness(citySearch) {
 		var placeFind =
@@ -35,33 +33,46 @@ jQuery.ajaxPrefilter(function (options) {
 			method: "GET"
 		})
 			.then(function (response) {
+				console.log(("response"));
 				console.log((response));
 				var placesArray = response.results;
-				var i; 
-				for(i=0; i < 4; i++) {
-			
+				var i;
+				for (i = 0; i < 4; i++) {
 
-				var placeDetails =
-					"https://maps.googleapis.com/maps/api/place/details/json?place_id=" + placesArray[i].place_id + "&key=" + APIKey;
 
-				$.ajax({
-					url: placeDetails,
-					method: "GET"
+					var placeDetails =
+						"https://maps.googleapis.com/maps/api/place/details/json?place_id=" + placesArray[i].place_id + "&key=" + APIKey;
+					console.log("arrayList[i}");
+					$.ajax({
+						url: placeDetails,
+						method: "GET"
 
-					//return details and format response in HTML
-				}).then(function (response) {
-					console.log(response);
-					//to avoid repeating city information on button click
-					$("card-action").empty();
-					$("").empty();
+						//return details and format response in HTML
+					}).then(function (response) {
 
-					var image = $('<img class="imgsize">').attr("src", "http:");
-					var ispName = $("<p>").text("ISP:" + response.name + "<p>");
-					var phoneNumber = $("<p>").text("Phone: " + response.formatted_phone_number + "<p>");
-					var webAddress = $("<p>").text("Website: " + response.website + "<p>");
-					var custRating = $("<p>").text("Customer rating (1-5): " + response.rating + "<p>");
-				})
-			}
+						console.log("========================================================");
+
+						console.log("name");
+						console.log(response.result.name);
+
+						console.log("formatted_phone_number");
+						console.log(response.result.formatted_phone_number);
+
+						console.log("website");
+						console.log(response.result.website);
+
+						console.log("rating");
+						console.log(response.result.rating);
+
+						var image = $('<img class="imgsize">').attr("src", "./assets/images/wifiSymbol.png");
+						var ispName = $("<p>").text("ISP:" + response.result.name + "<p>");
+						var phoneNumber = $("<p>").text("Phone: " + response.result.formatted_phone_number + "<p>");
+						var webAddress = $("<p>").text("Website: " + response.result.website + "<p>");
+						var custRating = $("<p>").text("Customer rating (1-5): " + response.result.rating + "<p>");
+						// };
+
+					})
+				};
 			});
 
 	}
